9242f70987150b42b9b73f728d582932a4d49d67
Alfred Birketvedt <alfredbirk@gmail.com>
2020-03-05 20:55:27 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useCountUp } from 'react-countup';
+import CountUp from 'react-countup';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({ hits: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://www.vg.no/spesial/2020/corona-viruset/data/norway/',
+      );
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
+  
+  if (!data.totals) {
+    return null
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Container">
+          <CountUp start={0} end={data.totals.confirmed} delay={0}>
+            {({ countUpRef }) => (
+              <div>
+                <div className="Number" ref={countUpRef} />
+              </div>
+            )}
+          </CountUp>
+          <h1 className="Description">Personer smittet av Corona i Norge</h1>
+        </div>
       </header>
     </div>
   );
